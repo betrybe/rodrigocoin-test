521f245e3629f304a980bdb68a39fc021a48a36b
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2021-09-10 11:19:45 -0400

    Fixing code format (removed spaces from empty lines)

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -7,24 +7,24 @@ class CompleteReport(SimpleReport):
         "Produtos estocados por empresa:\n"
         "{prods_per_comp}"
     )
-    
+
     PRODS_PER_COMP_FMT = "- {company}: {count}\n"
 
     @classmethod
     def generate(cls, data):
         prods_count = cls._get_prods_count(data)
-        
+
         simple_report = super()._generate(
             oldest_fab=cls._find_oldest_fab(data),
             closest_exp=cls._find_closest_exp(data),
             most_prods=cls._find_most_prod_comp(prods_count),
         )
-        
+
         return CompleteReport.REPORT_FMT.format(
             simple_report=simple_report,
             prods_per_comp=cls._generate_prods_per_comp(prods_count)
         )
-    
+
     @classmethod
     def _generate_prods_per_comp(cls, prods_count):
         return ''.join([
