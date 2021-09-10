a8733ff9e9f3a39eca0e6cc9b61f00b7fe254df5
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2021-09-10 11:15:27 -0400

    Refactoring module simple_report to allow for easier inheritance

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, OrderedDict
 from datetime import date
 
 
@@ -9,16 +9,24 @@ class SimpleReport:
         "Empresa com maior quantidade de produtos estocados: {most_prods}\n"
     )
 
-    @staticmethod
-    def generate(data):
-        return SimpleReport.REPORT_FMT.format(
-            oldest_fab=SimpleReport._find_oldest_fab(data),
-            closest_exp=SimpleReport._find_closest_exp(data),
-            most_prods=SimpleReport._find_most_prod_comp(data),
+    @classmethod
+    def generate(cls, data):
+        return cls._generate(
+            oldest_fab=cls._find_oldest_fab(data),
+            closest_exp=cls._find_closest_exp(data),
+            most_prods=cls._find_most_prod_comp(cls._get_prods_count(data)),
         )
 
-    @staticmethod
-    def _find_oldest_fab(data):
+    @classmethod
+    def _generate(cls, oldest_fab, closest_exp, most_prods):
+        return SimpleReport.REPORT_FMT.format(
+            oldest_fab=oldest_fab,
+            closest_exp=closest_exp,
+            most_prods=most_prods,
+        )
+
+    @classmethod
+    def _find_oldest_fab(cls, data):
         fab_dates = [
             date.fromisoformat(item['data_de_fabricacao'])
             for item in data
@@ -26,8 +34,8 @@ class SimpleReport:
 
         return min(fab_dates).isoformat()
 
-    @staticmethod
-    def _find_closest_exp(data):
+    @classmethod
+    def _find_closest_exp(cls, data):
         exp_dates = [
             date.fromisoformat(item['data_de_validade'])
             for item in data
@@ -38,13 +46,26 @@ class SimpleReport:
 
         return min(exp_dates).isoformat()
 
-    @staticmethod
-    def _find_most_prod_comp(data):
-        product_count = Counter([
+    @classmethod
+    def _find_most_prod_comp(cls, prods_count):
+        # most_common always returns a list with a tuple (key, count)
+        # so we get the top item and return the key
+        return prods_count.most_common(1)[0][0]
+
+    @classmethod
+    def _get_prods_count(cls, data):
+        return OrderedCounter([
             item['nome_da_empresa']
             for item in data
         ])
 
-        # most_common always returns a list with a tuple (key, count)
-        # so we get the top item and return the key
-        return product_count.most_common(1)[0][0]
+
+# Class created from python documentation
+class OrderedCounter(Counter, OrderedDict):
+    'Counter that remembers the order elements are first encountered'
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
+
+    def __reduce__(self):
+        return self.__class__, (OrderedDict(self),)
