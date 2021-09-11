3586ec6b2fa874966bf2cc7b4de3a8b8551ef241
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2021-09-11 09:27:17 -0400

    Adding docstring to importer code

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -6,6 +6,16 @@ from inventory_report.importer.importer import Importer, assert_file_extension
 class CsvImporter(Importer):
     @staticmethod
     def import_data(filename):
+        """
+        Imports data from CSV file.
+
+        Args:
+            filename (str): path to filename
+
+        Returns:
+            list: a list of items (dict) loaded from the file
+        """
+
         assert_file_extension(filename, 'csv')
 
         with open(filename) as f:
--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -6,6 +6,16 @@ from inventory_report.importer.importer import Importer, assert_file_extension
 class JsonImporter(Importer):
     @staticmethod
     def import_data(filename):
+        """
+        Imports data from JSON file.
+
+        Args:
+            filename (str): path to filename
+
+        Returns:
+            list: a list of items (dict) loaded from the file
+        """
+
         assert_file_extension(filename, 'json')
 
         with open(filename) as f:
