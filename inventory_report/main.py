abc68b04aa5db1d6585585136525fbe8de38f8c8
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2021-09-10 20:05:30 -0400

    Implementing command-line main method

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,33 @@
+import sys
+
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+EXT_IMPORTER = {
+    'csv': CsvImporter,
+    'json': JsonImporter,
+    'xml': XmlImporter
+}
+
+
+def _print_report(filename, report_type):
+    ext = filename.split('.')[-1].lower()
+    importer = EXT_IMPORTER[ext]
+
+    inventory = InventoryRefactor(importer)
+    report = inventory.import_data(filename, report_type)
+
+    print(report, end='')
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        print("Verifique os argumentos", file=sys.stderr)
+        return
+
+    _print_report(
+        filename=sys.argv[1],
+        report_type=sys.argv[2]
+    )
